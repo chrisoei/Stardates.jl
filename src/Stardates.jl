e1e4ec02d20be4b291215a0658b7b5e408f95037
Chris Oei <chris.oei@gmail.com>
2019-10-09 10:21:21 -0700

    Files generated by PkgTemplates

---
+++ src/Stardates.jl
@@ -0,0 +1,5 @@
+module Stardates
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Stardates
+using Test
+
+@testset "Stardates.jl" begin
+    # Write your own tests here.
+end
