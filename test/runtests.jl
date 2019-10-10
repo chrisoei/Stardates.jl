4f137182d38136e90f487f61bad0a6145117f9f6
Chris Oei <chris.oei@gmail.com>
2019-10-09 17:58:26 -0700

    Add test for converting stat mtime to stardate

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,4 +14,17 @@ using TimeZones
         2014.747978420491791) < 3.0 * Stardates.second
     @test abs(nyse_closing_stardate("2014-09-30") - 2014.747488584474922) <
         3.0 * Stardates.second
+    @test abs(SD(DateTime(2012, 1, 2, 3, 4, 5)).sd -
+            2012.0030815181644) < 3.0 * Stardates.second
+
+    sd1 = SD()
+    sleep(1.0)
+    f1 = tempname()
+    write(f1, "Hello")
+    sd2 = SD(stat(f1))
+    rm(f1)
+    sleep(1.0)
+    sd3 = SD()
+    @test sd1.sd <= sd2.sd
+    @test sd2.sd <= sd3.sd
 end
